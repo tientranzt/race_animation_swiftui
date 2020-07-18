0be17cbd20b9e6eb2eb0b24235a53dcf89f6a157
tientran <tientran.home@gmail.com>
2020-07-18 20:13:08 +0700

    push first

--- animation_swiftui/ContentView.swift
+++ animation_swiftui/ContentView.swift
@@ -9,8 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isRace : Bool = false
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack(alignment:.top){
+                VStack{Text("🐝").font(.system(size: 80))}
+                    .offset(x: 0, y: self.isRace ? 600 : 0)
+                    .animation(Animation.easeIn(duration: 5))
+                Divider()
+                VStack{Text("🐌").font(.system(size: 80))}
+                    .offset(x: 0, y: self.isRace ? 600 : 0)
+                    .animation(Animation.easeIn(duration: 3))
+                Divider()
+                VStack{Text("🐴").font(.system(size: 80))}
+                    .offset(x: 0, y: self.isRace ? 600 : 0)
+                    .animation(Animation.easeIn(duration: 8))
+                
+                Divider()
+                VStack{Text("🐢").font(.system(size: 80))}
+                    .offset(x: 0, y: self.isRace ? 600 : 0)
+                    .animation(Animation.easeIn(duration: 12))
+                
+            }
+            Spacer()
+            Button(action: {
+                self.isRace.toggle()
+            }){
+                Text("Race").bold()
+            }
+            .padding()
+            .background(Color.red)
+            .foregroundColor(.white)
+            .font(.system(size: 20))
+        }
     }
 }
 
