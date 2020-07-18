f61e0a89a0d5a0d1a11a26626dc0f5b06b9e0e03
tientran <tientran.home@gmail.com>
2020-07-18 19:47:25 +0700

    Initial Commit

---
+++ animation_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  animation_swiftui
+//
+//  Created by tientran on 7/18/20.
+//  Copyright © 2020 tientran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
